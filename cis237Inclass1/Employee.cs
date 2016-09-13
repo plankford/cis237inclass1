5a978ed92c1ae1aa044715e6fd3ede9fcffb5e63
Patrick Lankford <plankfordusmc@gmail.com>
2016-09-13 16:52:21 -0400

    Add Employee class and some code to main

---
+++ cis237Inclass1/Employee.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cis237Inclass1
+{
+    class Employee
+    {
+        //**************************
+        // Backing Fields / Variables
+        //**************************
+        private string _firstName;
+        private string _lastName;
+        private decimal _weeklySalary;
+
+        //**************************
+        // Properties
+        //**************************
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public decimal WeeklySalary
+        {
+            get { return _weeklySalary; }
+            set { _weeklySalary = value; }
+        }
+
+        //*******************************
+        // Public Methods
+        //*******************************
+        public override string ToString()   // this is replacing the ToString method that comes with the class with this new one
+        {
+            // "this" will refer to anything that is defined in the class
+            return this._firstName + " " + this._lastName;
+        }
+
+        public decimal YearlySalary()
+        {
+            return this._weeklySalary * 52;
+        }
+
+        //******************************
+        // Constructor
+        //******************************
+        public Employee(string firstName, string lastName, decimal weeklySalary)
+        {
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._weeklySalary = weeklySalary;
+        }
+
+        public Employee()
+        {
+            //Do Nothing
+            //Default Constructor that will do nothing
+        }
+    }
+}
--- cis237Inclass1/Program.cs
+++ cis237Inclass1/Program.cs
@@ -10,6 +10,31 @@ namespace cis237Inclass1
     {
         static void Main(string[] args)
         {
+            Employee myEmployee = new Employee();
+            myEmployee.FirstName = "Patrick";
+            myEmployee.LastName = "Lankford";
+            myEmployee.WeeklySalary = 2010.56m;
+
+            Console.WriteLine(myEmployee.FirstName);
+            Console.WriteLine(myEmployee);
+
+            //Create the array
+            Employee[] employees = new Employee[10];
+
+            //Add the employee to the array
+            employees[0] = new Employee("James", "Kirk", 453.00m);
+            employees[1] = new Employee("Alice", "Cooper", 290.00m);
+            employees[2] = new Employee("Tonya", "Harding", 800.00m);
+            employees[3] = new Employee("Tony", "Danza", 750.00m);
+            employees[4] = new Employee("Leroy", "Jenkins", 320.00m);
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    Console.WriteLine(employee.ToString() + " " + employee.YearlySalary());
+                }           
+            }
         }
     }
 }
